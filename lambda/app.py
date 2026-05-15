4a93c1ce05cdb89c8b2ac9a23f1655aa7537bf5d
wilfriedbako <bakowilfriedmarcel@gmail/com>
2026-05-15 03:01:40 -0400

    Update Bedrock model

--- lambda/app.py
+++ lambda/app.py
@@ -37,7 +37,7 @@ Return ONLY valid JSON like this:
     try:
 
         response = bedrock.invoke_model(
-            modelId="amazon.titan-text-lite-v1",
+            modelId="amazon.titan-text-express-v1",
             body=json.dumps({
                 "inputText": prompt,
                 "textGenerationConfig": {
